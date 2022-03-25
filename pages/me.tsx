ed1138edd9b57d900f1f7a29770d78cdd5e9da01
Jimmyboy <devJimmyboy@gmail.com>
2022-03-25 17:23:53 -0500

    Fix issues w/ user pages & remove storybook

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,34 +1,19 @@
-import {
-  Avatar,
-  Box,
-  Container,
-  Divider,
-  Grid,
-  IconButton,
-  Stack,
-  Tab,
-  TabProps,
-  Tabs,
-  TabsProps,
-  Tooltip,
-  Typography,
-} from "@mui/material"
-import { NextPage } from "next"
-import { User } from "next-auth"
+import { Icon } from "@iconify/react";
+import { Avatar, Grid, IconButton, Stack, Tab, TabProps, Tabs, TabsProps, Tooltip, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { gsap } from "gsap";
+import { NextPage } from "next";
+import { User } from "next-auth";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useLayoutEffect } from "react";
 
-import { gsap } from "gsap"
-import React, { useEffect, useLayoutEffect } from "react"
-import { Icon } from "@iconify/react"
-import { useRouter } from "next/router"
-import { styled } from "@mui/material/styles"
-import { useSmash } from "../lib/SmashContext"
-import Link from "../src/Link"
-import PokemonSquare from "../components/PokemonSquare"
-import Image from "next/image"
-import { ScoreDisplay } from "../components/SmashPass"
-import Head from "next/head"
+import PokemonSquare from "../components/PokemonSquare";
+import { ScoreDisplay } from "../components/SmashPass";
+import { useSmash } from "../lib/SmashContext";
+import Link from "../src/Link";
 
-const ScoreHolder = styled("div")`
+const ScoreHolder = styled('div')`
   background-position: center;
   background-size: 80%;
   background-repeat: no-repeat;
@@ -46,7 +31,7 @@ interface Error {
 }
 
 type UserProp = User | string
-type ScoreProp = { choices: { [key: string]: "smash" | "pass" }; smashCount: number; passCount: number }
+type ScoreProp = { choices: { [key: string]: 'smash' | 'pass' }; smashCount: number; passCount: number }
 type ScoreError = string
 interface Props {}
 
@@ -55,30 +40,24 @@ const tl = gsap.timeline({ repeat: -1 })
 const UserProfile: NextPage<Props> = ({}) => {
   const { style, score } = useSmash()
   const pictureBgRef = React.useRef<HTMLDivElement>(null)
-  const router = useRouter()
 
   const [tab, setTab] = React.useState<number>(1)
-  const [numSmashed, setNum] = React.useState(0)
   const user: Partial<UserProp> = {
-    name: "Anonymous (You)",
-    profileImageUrl: "",
-    displayName: "Anonymous (You)",
-    username: "Anonymous",
+    name: 'Anonymous (You)',
+    profileImageUrl: '',
+    displayName: 'Anonymous (You)',
+    username: 'Anonymous',
   }
 
   useEffect(() => {
     if (pictureBgRef.current) {
-      tl.fromTo(
-        pictureBgRef.current,
-        { scale: 1, autoAlpha: 1, borderWidth: 8 },
-        { scale: 1.25, autoAlpha: 0, duration: 2, borderWidth: 0, ease: "cubic.bounce" }
-      )
+      tl.fromTo(pictureBgRef.current, { scale: 1, autoAlpha: 1, borderWidth: 8 }, { scale: 1.25, autoAlpha: 0, duration: 2, borderWidth: 0, ease: 'cubic.bounce' })
       tl.play()
     }
   }, [pictureBgRef])
 
   if (!score) {
-    return <div className="w-full h-full flex flex-col items-center justify-center">{"Unknown Error Occured."}</div>
+    return <div className="w-full h-full flex flex-col items-center justify-center">{'Unknown Error Occured.'}</div>
   } else
     return (
       <>
@@ -87,92 +66,62 @@ const UserProfile: NextPage<Props> = ({}) => {
           <meta name="title" content={`PokeSmash - Your Stats`} />
           <meta name="description" content={`The user page for Yourself. Shows the Smashes and Passes you chose.`} />
         </Head>
-        <Stack
-          className="poke-scrollbar"
-          sx={{ overflowX: "hidden", maxHeight: "100vh", minHeight: "1080px" }}
-          direction="column"
-          p={6}
-          pb={2}
-          alignItems="center">
-          <Tooltip className="absolute top-24 left-4" title="Go Back">
+        <Stack className="poke-scrollbar" sx={{ overflow: 'hidden', height: '100vh' }} direction="column" p={6} pb={2} alignItems="center">
+          <Tooltip className="absolute top-12 md:top-24 left-4" title="Go Back">
             <IconButton className="fancy-bg w-10 h-10 p-0 m-0" LinkComponent={Link} href="/">
               <Icon icon="fa-solid:arrow-left" />
             </IconButton>
           </Tooltip>
           <div className="flex flex-row gap-6 items-center w-11/12 my-2 md:my-4 lg:my-6 ">
-            <div
-              ref={pictureBgRef}
-              className="rounded-full absolute w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 border-purple-700"
-            />
-            <Avatar
-              className=" w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 "
-              alt={user?.name}
-              src={user?.profileImageUrl}
-            />
+            <div ref={pictureBgRef} className="rounded-full absolute w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 border-purple-700" />
+            <Avatar className=" w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 " alt={user?.name} src={user?.profileImageUrl} />
 
-            <Typography
-              fontSize={32}
-              className="border-purple-500 border-4 select-none flex flex-row items-center gap-2 border-solid rounded-lg px-4 bg-purple-500 text-white"
-              fontWeight={600}>
+            <Typography fontSize={32} className="border-purple-500 border-4 select-none flex flex-row items-center gap-2 border-solid rounded-lg px-4 bg-purple-500 text-white" fontWeight={600}>
               {user.displayName}
             </Typography>
             <div className="flex-grow" />
             <Typography fontSize={32} fontWeight={600} justifySelf="flex-end" alignSelf="flex-end">
-              {score.passes + score.smashes || "?"} / 868
+              {score.passes + score.smashes || '?'} / 868
             </Typography>
           </div>
-          <StyledTabs
-            value={tab}
-            variant="fullWidth"
-            onChange={(e, nTab) => setTab(nTab)}
-            sx={{ width: "100%" }}
-            aria-label="User's Stats, Smash list, and Pass list.">
-            <StyledTab
-              icon={<Image src="https://cdn.7tv.app/emote/60aeafcb229664e866bef5ac/4x" width={32} height={32} />}
-              label="Passes"
-              iconPosition="start"
-            />
-            <StyledTab
-              icon={<Image src="https://cdn.7tv.app/emote/611a4aac62a016377dd91a25/4x" width={32} height={32} />}
-              label="Stats"
-              iconPosition="start"
-            />
-            <StyledTab
-              icon={<Image src="https://cdn.7tv.app/emote/60b8cce455c320f0e89d3514/4x" width={32} height={32} />}
-              label="Smashes"
-              iconPosition="start"
-            />
+          <StyledTabs value={tab} variant="fullWidth" onChange={(e, nTab) => setTab(nTab)} sx={{ width: '100%' }} aria-label="User's Stats, Smash list, and Pass list.">
+            <StyledTab icon={<Image src="https://cdn.7tv.app/emote/60aeafcb229664e866bef5ac/4x" width={32} height={32} />} label="Passes" iconPosition="start" />
+            <StyledTab icon={<Image src="https://cdn.7tv.app/emote/611a4aac62a016377dd91a25/4x" width={32} height={32} />} label="Stats" iconPosition="start" />
+            <StyledTab icon={<Image src="https://cdn.7tv.app/emote/60b8cce455c320f0e89d3514/4x" width={32} height={32} />} label="Smashes" iconPosition="start" />
           </StyledTabs>
           <div
-            className="flex-grow w-full"
+            className="relative flex-grow w-full overflow-hidden"
             style={{
-              borderRadius: "0 0 0.625rem 0.625rem",
-              border: "1px solid #55df55",
-              borderTop: "none",
+              borderRadius: '0 0 0.625rem 0.625rem',
+              border: '1px solid #55df55',
+
+              borderTop: 'none',
             }}>
             {tab === 1 ? (
               <StatsPage smashes={score.smashes} passes={score.passes} />
             ) : (
-              <Grid
-                container
-                spacing={2}
-                p={0}
-                sx={{
-                  pt: 2,
-                  pl: 2,
-                  maxHeight: "100%",
-                  overflowX: "hidden",
-                  overflowY: "scroll",
-                }}
-                columns={{ xs: 8, sm: 18, md: 24, lg: 60 }}>
-                {Object.keys(score.choices)
-                  .filter((val) => score.choices[val] === (tab === 2 ? "smash" : "pass"))
-                  .map((id) => (
-                    <Grid item xs={2} sm={3} md={3} lg={4} key={id}>
-                      <PokemonSquare i={Number(id)} choice={tab === 2 ? "smash" : "pass"} style={style || "showdown"} />
-                    </Grid>
-                  ))}
-              </Grid>
+              <div className="absolute h-full overflow-y-auto">
+                <Grid
+                  container
+                  alignItems="center"
+                  justifyContent="center"
+                  spacing={2}
+                  sx={{
+                    p: 2,
+                    overflowX: 'hidden',
+                    overflowY: 'scroll',
+                    maxHeight: 'content',
+                  }}
+                  columns={{ xs: 8, sm: 18, md: 24, lg: 60 }}>
+                  {Object.keys(score.choices)
+                    .filter((val) => score.choices[val] === (tab === 2 ? 'smash' : 'pass'))
+                    .map((id) => (
+                      <Grid item xs={2} sm={3} md={3} lg={4} key={id}>
+                        <PokemonSquare i={Number(id)} choice={tab === 2 ? 'smash' : 'pass'} style={style || 'showdown'} />
+                      </Grid>
+                    ))}
+                </Grid>
+              </div>
             )}
           </div>
         </Stack>
@@ -187,25 +136,23 @@ interface StyledTabsProps extends TabsProps {
   onChange: (event: React.SyntheticEvent, newValue: number) => void
 }
 
-const StyledTabs = styled((props: StyledTabsProps) => (
-  <Tabs {...props} TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }} />
-))({
-  minHeight: "4em",
+const StyledTabs = styled((props: StyledTabsProps) => <Tabs {...props} TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }} />)({
+  minHeight: '4em',
   "& div[role='tablist']": {
-    gap: "0.25rem",
+    gap: '0.25rem',
 
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
   },
-  borderBottom: "1px solid #55df55",
-  "& .MuiTabs-indicator": {
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "transparent",
+  borderBottom: '1px solid #55df55',
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
   },
-  "& .MuiTabs-indicatorSpan": {
+  '& .MuiTabs-indicatorSpan': {
     maxWidth: 40,
-    width: "100%",
-    backgroundColor: "#635ee7",
+    width: '100%',
+    backgroundColor: '#635ee7',
   },
 })
 
@@ -214,24 +161,24 @@ interface StyledTabProps extends TabProps {
 }
 
 const StyledTab = styled((props: StyledTabProps) => <Tab {...props} />)(({ theme }) => ({
-  border: "1px solid #55df55",
-  borderBottom: "none",
-  transition: "all 0.2s",
-  borderRadius: "0.625rem 0.625rem 0 0",
-  display: "flex",
-  flexDirection: "row",
+  border: '1px solid #55df55',
+  borderBottom: 'none',
+  transition: 'all 0.2s',
+  borderRadius: '0.625rem 0.625rem 0 0',
+  display: 'flex',
+  flexDirection: 'row',
   gap: 4,
-  boxShadow: "none",
-  textTransform: "none",
+  boxShadow: 'none',
+  textTransform: 'none',
   fontWeight: theme.typography.fontWeightBold,
   fontSize: theme.typography.pxToRem(15),
-  color: "rgba(255, 255, 255, 0.7)",
-  "&.Mui-selected": {
-    color: "#fff",
+  color: 'rgba(255, 255, 255, 0.7)',
+  '&.Mui-selected': {
+    color: '#fff',
     borderWidth: 2,
   },
-  "&.Mui-focusVisible": {
-    backgroundColor: "rgba(100, 95, 228, 0.32)",
+  '&.Mui-focusVisible': {
+    backgroundColor: 'rgba(100, 95, 228, 0.32)',
   },
 }))
 
