913b18c40f1f7955886e238bf3fdbfd5497be7c3
Jimmyboy <devJimmyboy@gmail.com>
2022-03-29 20:24:33 -0500

    add better Icons for search & mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,6 +37,10 @@ export default function MyApp(props: MyAppProps) {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel="icon" type="image/png" href="/favicon.png" />
           <title>Pokémon Smash or Pass</title>
           <meta name="title" content="PokeSmash - Pokémon Smash or Pass" />
